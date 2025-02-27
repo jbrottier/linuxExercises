d6675da136fb60e027b60d83f858579e24d5b272
John Chumlea <jchumlea@wisc.edu>
2025-02-27 13:08:08 -0600

    v1.2, works I think

--- school.sh
+++ school.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-sum=0
-count=0
 
-cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f 7 | while read n; do
-    sum=$((sum + n))
-    count=$((count + 1))
-done
-if [ "$count" -gt 0 ]; then
-    avg=$((sum / count))
-else
-    avg=0
-fi
-     echo "$sum,$avg" >> Property_Tax_Roll.csv
-	   
-#v2.1
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d',' -f7 |{
+    sum=0
+    count=0
+    while read n; do
+	sum=$((sum + n))
+	count=$((count + 1))
+    done
+
+    echo "sum: $sum"
+    echo -n "avg: "
+    echo "scale=2; $sum / $count" | bc
+}
+
+    
+#v1.2
    
 
 #{		 sum = 0; while read n; do sum$(($sum + $n)); done; echo $sum;} #finds the sum
