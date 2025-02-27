2d4fbabd7dcd168d6a871e4411c7365e4a77f72f
John Chumlea <jchumlea@wisc.edu>
2025-02-27 09:32:27 -0600

    v1.1, not done

--- school.sh
+++ school.sh
@@ -3,18 +3,18 @@
 sum=0
 count=0
 
-grep "MADISON SCHOOLS" Property_Tax_Roll.csv | cut -d ',' --complement -f 7 | while read n; do
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d ',' -f 7 | while read n; do
     sum=$((sum + n))
     count=$((count + 1))
 done
-if [$count -gt 0]; then
-    avg=$((sum/count))
+if [ "$count" -gt 0 ]; then
+    avg=$((sum / count))
 else
     avg=0
 fi
      echo "$sum,$avg" >> Property_Tax_Roll.csv
 	   
-
+#v2.1
    
 
 #{		 sum = 0; while read n; do sum$(($sum + $n)); done; echo $sum;} #finds the sum
