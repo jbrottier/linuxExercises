8cca609f604d28b858b41a7b8392fbb78e187ed6
Samuel Negus <snegus@wisc.edu>
2025-03-04 08:08:18 -0600

    Finished five_dirs.sh

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+mkdir five
+for i in $(seq 5); do
+    mkdir "five/dir$i"
+    for j in $(seq 4); do
+	for n in $(seq $j); do
+	    echo "$j"
+	done > "five/dir$i/file$j"
+    done
+done
+# rm -r five; ./five_dirs.sh
